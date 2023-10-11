ec4b17fd35435e4501f0aed638c04c2cb98720be
Sam McLeod <sammcj@users.noreply.github.com>
2023-10-11 14:21:48 +1100

    minor updates

--- src/config.js
+++ src/config.js
@@ -228,7 +228,6 @@ export async function parseConfig() {
 
 					if (result[repo.uniqueName] !== undefined) {
 						result[repo.uniqueName].files.push(...files)
-						result[repo.uniqueName].commitPrefix.push(...commitPrefix)
 						return
 					}
 
