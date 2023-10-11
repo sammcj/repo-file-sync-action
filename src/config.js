71e09bcdf504cc53d8b6f57c55d394aa3bf20bed
Sam McLeod <sammcj@users.noreply.github.com>
2023-10-11 13:10:44 +1100

    minor updates

--- src/config.js
+++ src/config.js
@@ -191,7 +191,7 @@ const parseFiles = (files) => {
 				template: item.template === undefined ? TEMPLATE_DEFAULT : item.template,
 				replace: item.replace === undefined ? REPLACE_DEFAULT : item.replace,
 				deleteOrphaned: item.deleteOrphaned === undefined ? DELETE_ORPHANED_DEFAULT : item.deleteOrphaned,
-				commitPrefix: item.commitPrefix === undefined ? context.COMMIT_PREFIX : item.commitPrefix,
+				commitPrefix: item.commitPrefix || context.COMMIT_PREFIX,
 				exclude: parseExclude(item.exclude, item.source)
 			}
 		}
